027c9255b0d46a13dd9cf3e22fdfea139eb69e27
Ray X <rayxi2.71828@outlook.com>
2020-05-17 10:14:49 +0700

    Tiny clean-up

--- scripts/99BottlesOfBeer.main.kts
+++ scripts/99BottlesOfBeer.main.kts
@@ -4,7 +4,9 @@
 @file:DependsOn("org.jsoup:jsoup:1.13.1")
 
 val lyricsPageDocument = org.jsoup.Jsoup.connect("http://99-bottles-of-beer.net/lyrics.html").get()
-val newLine = System.lineSeparator()
+
+val lineSep = System.lineSeparator()
+
 lyricsPageDocument.select("div[id=\"main\"] p").map {
-    p -> p.textNodes().joinToString(separator=newLine) { it.toString().trim() }
-}.joinToString(separator=newLine.repeat(2))
+    p -> p.textNodes().joinToString(separator=lineSep) { it.toString().trim() }
+}.joinToString(separator=lineSep.repeat(2))
